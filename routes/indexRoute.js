88599ecfbc1ff4900672073c09d0c1fab77d782c
ntrix <ntrix@ntien.com>
2020-11-24 19:16:32 +0100

    routes

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const postController = require('../controllers/postController');
 const { check } = require('express-validator');
 const passport = require('passport');
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const userController = require('../controllers/userController');
 const requestController = require('../controllers/requestController');
 const postController = require('../controllers/postController');
